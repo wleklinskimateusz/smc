4a32931f9e38d7b096adc591bdb3c1729c8e4fe9
wleklinskimateusz <wleklinskimateusz05@gmail.com>
2024-10-21 22:34:20 +0200

    test(lexer): add tests for lexer

--- src/parser/lexical_analyzer.rs
+++ src/parser/lexical_analyzer.rs
@@ -1,3 +1,5 @@
+use std::char;
+
 pub trait TokenCollector {
     fn open_brace(&mut self, line: u32, position: u32);
     fn close_brace(&mut self, line: u32, position: u32);
@@ -26,12 +28,13 @@ impl LexicalAnalyzer {
         }
     }
 
-    pub fn lex(&mut self, s: String) {
+    pub fn lex(&mut self, s: &str) {
         self.line_number = 1;
         let lines = s.split("\n");
         for line in lines {
             self.lex_line(line);
             self.line_number += 1;
+            self.position = 0;
         }
     }
 
@@ -123,5 +126,137 @@ impl LexicalAnalyzer {
         } else {
             false
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{cell::RefCell, rc::Rc};
+
+    use super::*;
+
+    #[test]
+    fn test_empty_line() {
+        assert_lines("", "");
+    }
+
+    #[test]
+    fn test_whitespace_line() {
+        assert_lines("  ", "");
+    }
+
+    #[test]
+    fn test_single_char_tokens() {
+        assert_lines("{", "{");
+        assert_lines("}", "}");
+        assert_lines("(", "(");
+        assert_lines(")", ")");
+        assert_lines("<", "<");
+        assert_lines(">", ">");
+        assert_lines("-", "-");
+        assert_lines(":", ":");
+    }
+
+    #[test]
+    fn test_name() {
+        assert_lines("name", "name");
+        assert_lines("name1", "name1");
+        assert_lines("name_1", "name_1");
+        assert_lines("name 1", "name,1");
+    }
+
+    #[test]
+    fn test_assert_combination() {
+        assert_lines("{ name }", "{,name,}");
+        assert_lines("{     name1 }", "{,name1,}");
+        assert_lines("{ name_1   }", "{,name_1,}");
+        assert_lines("{ name 1       }", "{,name,1,}");
+    }
+
+    #[test]
+    fn test_multiline() {
+        assert_lines("{ name }\n{ name1 }", "{,name,},{,name1,}");
+    }
+
+    #[test]
+    fn test_error() {
+        assert_lines("{ name }\n{ name1 } %", "{,name,},{,name1,},ERROR");
+        assert_lines(
+            "{ name }\n{ name1 } %$ ^ ",
+            "{,name,},{,name1,},ERROR,ERROR,ERROR",
+        );
+    }
+
+    fn assert_lines(lines: &str, expected: &str) {
+        let tokens = Rc::new(RefCell::new(String::new()));
+
+        // Mock struct defined inside the test
+        struct MockCollector {
+            tokens_ref: Rc<RefCell<String>>,
+        }
+
+        // Implement the TokenCollector trait for MockCollector
+        impl<'a> TokenCollector for MockCollector {
+            fn open_brace(&mut self, line: u32, position: u32) {
+                self.tokens_ref.borrow_mut().push_str("{");
+                self.tokens_ref.borrow_mut().push(',');
+            }
+
+            fn close_brace(&mut self, line: u32, position: u32) {
+                self.tokens_ref.borrow_mut().push('}');
+                self.tokens_ref.borrow_mut().push(',');
+            }
+
+            fn open_parenthesis(&mut self, line: u32, position: u32) {
+                self.tokens_ref.borrow_mut().push('(');
+                self.tokens_ref.borrow_mut().push(',');
+            }
+
+            fn close_parenthesis(&mut self, line: u32, position: u32) {
+                self.tokens_ref.borrow_mut().push(')');
+                self.tokens_ref.borrow_mut().push(',');
+            }
+
+            fn open_angle_bracket(&mut self, line: u32, position: u32) {
+                self.tokens_ref.borrow_mut().push('<');
+                self.tokens_ref.borrow_mut().push(',');
+            }
+
+            fn close_angle_bracket(&mut self, line: u32, position: u32) {
+                self.tokens_ref.borrow_mut().push('>');
+                self.tokens_ref.borrow_mut().push(',');
+            }
+
+            fn dash(&mut self, line: u32, position: u32) {
+                self.tokens_ref.borrow_mut().push('-');
+                self.tokens_ref.borrow_mut().push(',');
+            }
+
+            fn colon(&mut self, line: u32, position: u32) {
+                self.tokens_ref.borrow_mut().push(':');
+                self.tokens_ref.borrow_mut().push(',');
+            }
+
+            fn name(&mut self, name: &str, line: u32, position: u32) {
+                self.tokens_ref.borrow_mut().push_str(name);
+                self.tokens_ref.borrow_mut().push(',');
+            }
+
+            fn error(&mut self, message: &str, line: u32, position: u32) {
+                self.tokens_ref.borrow_mut().push_str("ERROR");
+                self.tokens_ref.borrow_mut().push(',');
+            }
+        }
+        let mock_collector = MockCollector {
+            tokens_ref: Rc::clone(&tokens),
+        };
+
+        let mut analyzer = LexicalAnalyzer::new(Box::new(mock_collector));
+        analyzer.lex(lines);
+        let mut tokens = tokens.borrow_mut();
+
+        tokens.pop();
+
+        assert_eq!(tokens.as_str(), expected);
     }
 }
